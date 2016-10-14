3bc9bf8d97ffc5cf9081907c8b4db4d4f0ab92e0
liujiajie <liujiajie@dianping.com>
2016-10-14 16:50:37 +0800

    【增加JJViewController】

---
+++ JJAwesomeMenuDemo/JJAwesomeMenuDemo/JJViewController.h
@@ -0,0 +1,13 @@
+//
+//  JJViewController.h
+//  JJAwesomeMenuDemo
+//
+//  Created by 刘佳杰 on 16/10/14.
+//  Copyright © 2016年 刘佳杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JJViewController : UIViewController
+
+@end
---
+++ JJAwesomeMenuDemo/JJAwesomeMenuDemo/JJViewController.m
@@ -0,0 +1,22 @@
+//
+//  JJViewController.m
+//  JJAwesomeMenuDemo
+//
+//  Created by 刘佳杰 on 16/10/14.
+//  Copyright © 2016年 刘佳杰. All rights reserved.
+//
+
+#import "JJViewController.h"
+
+@interface JJViewController ()
+
+@end
+
+@implementation JJViewController
+
+- (void)viewDidLoad {
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    self.navigationItem.title = @"导航栏";
+}
+
+@end
